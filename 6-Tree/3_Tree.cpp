a0bb22feda22b3d1d7f7218ecd4c293710000b00
surya <suryafirefly2001@gmail.com>
2023-02-27 00:36:52 +0530

    Sprial Level Order function

---
+++ 6-Tree/3_Tree.cpp
@@ -0,0 +1,114 @@
+
+#include<iostream>
+#include<cstdlib>
+#include<queue>
+#include<stack>
+#include<limits>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+// Sprial Level Order
+/*Method -1 using queue & stack
+Time Complexity = O(N)
+
+void sprialOrder(struct Node* root){
+
+    stack<struct Node*>st;
+    queue<struct Node*>qt;
+    qt.push(root);
+    bool rightToLeft = false;
+
+    while(!qt.empty()){
+        int n = qt.size();
+
+        for(int i=0;i<n;i++){
+
+            struct Node* node = qt.front();
+            qt.pop();
+            if(rightToLeft){
+                st.push(node);
+            }else{
+                cout<<node->data<<" ";
+            }
+
+            if(node->left != nullptr)
+                qt.push(node->left);
+            if(node->right != nullptr)
+                qt.push(node->right);
+        }
+
+        if(rightToLeft){
+            for(int i=0;i<n;i++){
+                cout<<st.top()->data<<" ";
+                st.pop();
+            }
+        }
+        
+        rightToLeft = !rightToLeft;
+        
+    }
+}
+*/
+
+// Method - 2
+void sprialOrder(struct Node* root){
+    stack<struct Node*> st1, st2;
+    st1.push(root);
+
+    while(!st1.empty() || !st2.empty()){
+        int n = st1.size();
+        for(int i=0;i<n;i++){
+            struct Node* node = st1.top();
+            st1.pop();
+            cout<<node->data<<" ";
+
+            if(node->left != nullptr)
+                st2.push(node->left);
+            if(node->right != nullptr)
+                st2.push(node->right); 
+        }
+
+        n = st2.size();
+        for(int i=0;i<n;i++){
+            struct Node* node = st2.top();
+            cout<<node->data<<" ";
+            st2.pop();
+
+            if(node->right != nullptr)
+                st1.push(node->right);
+
+            if(node->left != nullptr)
+                st1.push(node->left);
+        }
+
+    }
+}
+
+int main(){
+    struct Node* root = new Node(15);
+
+    root->left = new Node(8);
+    root->right = new Node(23);
+
+    root->left->left = new Node(4);
+    root->left->right = new Node(10);
+
+    root->right->left = new Node(19);
+    root->right->right = new Node(25);
+    root->right->right->left = new Node(24);
+
+    cout<<"\nSpiral Order of Binary Tree is ";
+    sprialOrder(root);
+
+    return 0;
+}
